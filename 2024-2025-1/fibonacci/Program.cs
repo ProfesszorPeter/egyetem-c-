63abd5739facad80a688bb4f5c3baf53a5609abb
ProfesszorPeter <simonpetymeg@gmail.com>
2024-10-31 08:57:13 +0100

    rekurzió

---
+++ 2024-2025-1/fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(100));
+
+            static int fib(int n)
+            {
+                if(n>1)
+                {
+                    return fib(n-2) + fib(n-1);
+                }
+                else
+                {
+                    return 1;
+                }
+
+            }
+        }
+    }
+}
+
---
+++ 2024-2025-1/hanoi/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void hanoi(int honnan, int manko, int hova, int n)
+            {
+                hanoi(1,2,3,3);
+                if(n>=1)
+                {
+                    hanoi(honnan, hova, manko, n-1);
+                    Console.WriteLine($"{honnan} -> {hova}");
+                    hanoi(manko,honnan,hova, n-1);
+                }
+            }
+        }
+    }
+}
+
