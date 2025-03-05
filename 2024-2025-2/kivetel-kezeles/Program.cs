f1d60bbff0af15573bea479272ad92e5acef3212
Simon Péter <simonpetymev@gmail.com>
2025-03-05 11:28:31 +0000

    kivetel-kezeles

---
+++ 2024-2025-2/kivetel-kezeles/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace felev2
+{
+    class Kivetel
+    {
+        
+        static void Main(string[] args)
+            {
+                //vannak veszélyes szituációk
+                //eddig:
+                /*
+                int y = 0;
+                int x = 10 / y;
+                Console.WriteLine(x);
+                
+                int[] t = {1,2,3,4,5};
+                Console.WriteLine(t[10]);
+
+                StreamReader be = new StreamReader("kutya.txt");
+                be.Close();
+                */
+
+                //megoldás
+                try {
+                int y = 1;
+                int x = 10 / y;
+                Console.WriteLine(x);
+
+                int[] t = {1,2,3,4,5};
+                Console.WriteLine(t[10]);
+
+                }
+                catch (DivideByZeroException e) {
+                    Console.WriteLine(("Nullával nem osztunk te Barom"));
+                    Console.WriteLine(e);
+                }
+                catch (IndexOutOfRangeException e) {
+                    Console.WriteLine("Mindennek van egy határa...");
+                    Console.WriteLine(e);
+                }
+
+                
+            }
+        }
+    }
