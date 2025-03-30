8488b0417cf5382836c676f1ae7dcac66ef40e87
Simon Péter <simonpetymeg@gmail.com>
2025-03-30 09:24:08 +0000

    Operárot felülírás

---
+++ 2024-2025-2/operatorPoint/Pont.cs
@@ -0,0 +1,29 @@
+﻿namespace felev2
+{
+    class Pont
+    {
+        public int X{get;}
+        public int Y{get;}
+
+        public Pont (int _X, int _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+        public static Pont operator+(Pont a, Pont b)
+        {
+            return new Pont(a.X+b.X,a.Y+b.Y);
+        }
+
+        public static Pont operator-(Pont a, Pont b)
+        {
+            return new Pont(a.X-b.X,a.Y-b.Y);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("X={0}, Y={1}",X,Y);
+        }
+    }
+
+}
---
+++ 2024-2025-2/operatorPoint/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace felev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pont p1 = new Pont(5,2);
+            Pont p2 = new Pont(-1,3);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+
+            Pont osszeg = p1 + p2;
+            Console.WriteLine(osszeg.ToString());
+
+            Pont kivonas = p1-p2;
+            Console.WriteLine(kivonas.ToString());
+        }
+    }
+}
