1a6e5b5d7ad23e5ae66a69951c607d8b995995a4
ProfesszorPeter <simonpetymeg@gmail.com>
2025-03-30 11:53:59 +0200

    tehetseg eleje

---
+++ 2024-2025-2/zh1gyak/tehetseg/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace felev2
+{
+    class Tehetseg
+    {
+        struct Adatok
+        {
+            public string Nev;
+            public int Kozonseg;
+            public int zsuritag1;
+            public int zsuritag2;
+            public int zsuritag3;
+            public int zsuritag4;
+        }
+        
+        //public string Nev;
+        //public int Kozonseg;
+        //public int zsuritag1;
+        //public int zsuritag2;
+        //public int zsuritag3;
+        //public int zsuritag4;
+
+        //public Tehetseg{"Kis István",1,2,3,4,5 }
+
+        static private void Main(string[] args)
+        {
+            StreamReader forras = new StreamReader("allomany.txt");
+            string resz = forras.ReadToEnd();
+            string[] reszadatok = resz.Split("\n");
+            int[] pontok = new int[30];
+
+            for (int i = 0; i<reszadatok.Length; i++)
+            {
+                Console.WriteLine("egy elem: {0}",reszadatok[i]);
+                Console.WriteLine();
+                //Adatok x;
+                //x.Nev = item[
+            }
+        }
+    }
+    
+}
+
