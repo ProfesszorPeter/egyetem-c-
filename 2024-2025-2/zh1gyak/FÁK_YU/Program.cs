6187fc1819e8efdc2f0a66712c06ee731c860855
ProfesszorPeter <simonpetymeg@gmail.com>
2025-03-31 19:11:16 +0200

    fák megoldás balázzsal(tuti be kell fejezni)

---
+++ 2024-2025-2/zh1gyak/FÁK_YU/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+namespace felev2
+{
+    class Fa
+    {
+        public string Nev;
+        public string faj;
+        public int kormeret;
+        public string telepules;
+        public int meres;
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Fa> adatok = new List<Fa>();
+            StreamReader forras = new StreamReader("fak.json");
+            string forrassor = forras.ReadToEnd();
+            adatok = JsonConvert.DeserializeObject<List<Fa>>(forrassor);
+
+            foreach (var elem in adatok) {
+                Console.WriteLine(elem.Nev);
+            }
+        }
+
+    }
+}
+
